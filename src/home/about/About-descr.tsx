ff3b108a3aba32820c9905a839b7b247df53b6cf
susi189 <ssteinhoff82@gmail.com>
2023-01-18 10:33:37 -0500

    Update: added about me description

--- src/home/about/About-descr.tsx
+++ src/home/about/About-descr.tsx
@@ -6,6 +6,14 @@ const About = () => {
     <div className={styles["about-descr"]}>
       <h1>Hi, I'm Susanna</h1>
       <h2>Full Stack Developer, Trust & Safety Expert, and Problem Solver</h2>
+      <div className={styles.overlay}>
+        I enjoy building things from scratch, experimenting with new
+        technologies, and creating user facing products. I am an experienced
+        problem solver who believes that best solutions are achieved through
+        cross-team collaboration. As a software developer I will use my can-do
+        attitude and my skills to contribute to the success of your company.
+        Let’s talk!
+      </div>
     </div>
   );
 };
